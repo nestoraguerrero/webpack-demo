26b840fcafe1edc46c9711b8285c7264833232a4
Nestor Guerrero <n.a.guerrero@tcu.edu>
2020-05-01 18:14:38 -0500

    tutorial3_changes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+import printMe from './print.js';
 
+  function component() {
+    const element = document.createElement('div');
+    const btn = document.createElement('button');
 
-function component() {
-  const element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+ 
+    element.appendChild(btn);
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
+    return element;
+  }
 
-  element.appendChild(myIcon);
-
-   console.log(Data);
-
-  return element;
-}
-
-document.body.appendChild(component());
+  document.body.appendChild(component());
