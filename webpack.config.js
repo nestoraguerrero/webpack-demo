8cd3cd0871d70bc9c58cefbf4213fbafd58ab72f
Nestor Guerrero <n.a.guerrero@tcu.edu>
2020-05-01 18:58:46 -0500

    tutorial5_changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,33 @@
 const path = require('path');
-  const HtmlWebpackPlugin = require('html-webpack-plugin');
-  const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-  module.exports = {
-    mode: 'development',
-    entry: {
-      app: './src/index.js',
-      print: './src/print.js',
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
     },
-    devtool: 'inline-source-map',
-    devServer: {
-      contentBase: './dist',
-      },
-    devServer: {
-      contentBase: './dist',
-      },
-    plugins: [
-      // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
-      new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-      new HtmlWebpackPlugin({
-        title: 'Development',
-      }),
-    ],
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: '/',
-    },
-  };
+  plugins: [
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Hot Module Replacement',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
